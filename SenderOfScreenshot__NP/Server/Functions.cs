178c4ff48e982c02ada436d5438f6973fa1f2bcf
ismayilkerimzade <ismayil.programmer@gmail.com>
2023-11-27 10:25:06 +0400

    Screenshot is ready

---
+++ SenderOfScreenshot__NP/Server/Functions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal static class Functions
+    {
+        public static Bitmap GetScreenshot()
+        {
+            using Bitmap screenshot = new Bitmap(Console.WindowWidth, Console.WindowHeight);
+
+            using (Graphics g = Graphics.FromImage(screenshot))
+            {
+                g.CopyFromScreen(Console.WindowLeft, Console.WindowTop, 0, 0, screenshot.Size);
+            }
+
+            screenshot.Save("screenshot.png", ImageFormat.Png);
+
+            return screenshot;
+        }
+
+        
+    }
+}
