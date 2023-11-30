59e663e393d2d9aec5568e9408cb4c638be106fe
ismayilkerimzade <ismayil.programmer@gmail.com>
2023-11-30 16:06:55 +0400

    Finish project

--- SenderOfScreenshot__NP/Client/MainWindow.xaml.cs
+++ SenderOfScreenshot__NP/Client/MainWindow.xaml.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
 using System.Windows.Media;
+using System.Drawing;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Client
 {
@@ -24,5 +25,69 @@ namespace Client
         {
             InitializeComponent();
         }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new Socket(AddressFamily.InterNetwork,
+                        SocketType.Dgram,
+                        ProtocolType.Udp);
+
+            var ip = IPAddress.Parse("10.10.144.177");
+            var port = 27001;
+            var remoteEP = new IPEndPoint(ip, port);
+
+            var buffer = Array.Empty<byte>();
+
+            buffer = Encoding.Default.GetBytes("screenshot");
+            client.SendTo(buffer, remoteEP);
+            int CountBytes = 0;
+            while (true)
+            {
+                var result = await client.ReceiveFromAsync(new ArraySegment<byte>(buffer),
+                                                     SocketFlags.None,
+                                                     remoteEP);
+                CountBytes = result.ReceivedBytes;
+                Image image = null;
+                var ListOfImageBytes = new List<byte>();
+
+                using (var ms = new MemoryStream())
+                {
+                    var TotalBytes = ms.ToArray().ToList();
+                    CountBytes = TotalBytes.Count;
+                    var BufferSize = ushort.MaxValue - 29;
+                    while (CountBytes > 0)
+                    {
+                        if (CountBytes < BufferSize)
+                        {
+                            BufferSize = CountBytes;
+                        }
+                        var CurrentlyBuffer = new byte[BufferSize];
+                        CurrentlyBuffer = TotalBytes.Skip(0).Take(BufferSize - 1).ToArray();
+                        ListOfImageBytes.AddRange(CurrentlyBuffer);
+                        TotalBytes.RemoveRange(0, BufferSize);
+                        CountBytes -= BufferSize;
+                    }
+
+                    image_Screenshot.Source = GetBitmapImageFromBytes(ListOfImageBytes.ToArray());
+
+                }
+
+            }
+        }
+
+
+        private BitmapImage GetBitmapImageFromBytes(byte[] imageBytes)
+        {
+            using (var imageStream = new MemoryStream(imageBytes))
+            {
+                BitmapImage bitmapImage = new();
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption= BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = imageStream;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+        
     }
 }
--- SenderOfScreenshot__NP/Server/Program.cs
+++ SenderOfScreenshot__NP/Server/Program.cs
@@ -1,3 +1,59 @@
-﻿using Server;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-Functions.GetScreenshot();
+var ServerListener = new Socket(AddressFamily.InterNetwork,
+                          SocketType.Dgram,
+                          ProtocolType.Udp);
+
+
+var ip = IPAddress.Parse("10.10.144.177");
+var port = 27001;
+var serverEP = new IPEndPoint(ip, port);
+
+ServerListener.Bind(serverEP);
+
+var buffer = new byte[ushort.MaxValue - 29];
+EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+var CountBytes = 0;
+var msg = string.Empty;
+
+while (true)
+{
+    var result = await ServerListener.ReceiveFromAsync(new ArraySegment<byte>(buffer),
+                                                 SocketFlags.None,
+                                                 remoteEP);
+
+    CountBytes = result.ReceivedBytes;
+    msg = Encoding.Default.GetString(buffer, 0, CountBytes);
+    if (msg == "screenshot")
+    {
+        while (true)
+        {
+            using Bitmap screenshot = new Bitmap(Console.WindowWidth, Console.WindowHeight);
+            using (var ms = new MemoryStream())
+            {
+                screenshot.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                var TotalBytes = ms.ToArray().ToList();
+                CountBytes = TotalBytes.Count;
+                var BufferSize = ushort.MaxValue - 29;
+                while (CountBytes > 0)
+                {
+                    if (CountBytes < BufferSize)
+                    {
+                        BufferSize = CountBytes;
+                    }
+                    var CurrentlyBuffer = new byte[BufferSize];
+                    CurrentlyBuffer = TotalBytes.Skip(0).Take(BufferSize - 1).ToArray();
+                    ServerListener.SendToAsync(CurrentlyBuffer,SocketFlags.None, remoteEP);
+                    TotalBytes.RemoveRange(0, BufferSize);
+                    CountBytes -= BufferSize; ;
+                }
+            }
+        }
+
+    }
+}
